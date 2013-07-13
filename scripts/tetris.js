c30782ba8285024ca366dca28a3112bb8ae3f18c
Buddy Williams <buddywilliams@gmail.com>
2013-07-13 16:24:56 -0400

    removed duplicate file

--- scripts/tetris.js
+++ scripts/tetris.js
@@ -133,14 +133,6 @@ function Tetris(canvasEl, fps, block_size) {
 		}
 	}
 
-	function getRandomArbitary (min, max) {
-		return Math.random() * (max - min) + min;
-	}
-
-	function getRandomInt(min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
-
 	this.initialize = function() {
 
 		document.onkeydown = function(e) { 
